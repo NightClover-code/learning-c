535e24477aecf94140deeb0e970e58e578acbb88
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-02-11 23:42:51 +0100

    learning about comments

---
+++ comments.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  // this is a comment
+  /*
+    this is a
+    multiline comment
+  */
+  printf("I like pizza \n");
+  printf("\"I like pizza\"- Sun tzu, Art of war, probably");
+
+  return 0;
+}
