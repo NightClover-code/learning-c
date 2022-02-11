6657714aa942ee84a7b49f75cc90db90ea0cf8b4
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-02-11 23:51:22 +0100

    variables

---
+++ README.md
@@ -0,0 +1,3 @@
+### ORDER
+- comments
+- variables
--- variables.c
+++ variables.c
@@ -2,6 +2,15 @@
 
 int main()
 {
+  int age = 16;         // integer
+  float gpa = 2.06;     // floating point nums
+  char grade = 'B';     // single character
+  char name[] = "Jack"; // array of characters (kinda like string)
+
+  printf("You are %d years old! \n", age);      // print int
+  printf("Hello %s! \n", name);                 // print string
+  printf("Your average grade is %c \n", grade); // print char
+  printf("Your gpa is %f \n", gpa);             // print float
 
   return 0;
 }
