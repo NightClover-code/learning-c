f3505c7dc37ebcc85f8ace408499d8faeb3f4d8d
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-02-12 00:34:54 +0100

    constants

---
+++ constants.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  /* Notes:
+        - a constant is a fixed value that cannot be changed later,
+          unlike most variables.
+        - It's common to use uppercasing for constants.
+  */
+
+  const float PI = 3.14159;
+
+  printf("%f", PI);
+
+  return 0;
+}
