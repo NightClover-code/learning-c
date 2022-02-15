a5d7275f751724963bb645b3caf0c123bbb404cd
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-02-15 22:48:09 +0100

    structuring code in a src folder

---
+++ src/inputs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  /* Notes:
+        - During division in this section, x / y result is a float, ig: 2/3.
+          Which means we need to store the result in a float, and change x or y to
+          become a float during the operation.
+  */
+
+  return 0;
+}
