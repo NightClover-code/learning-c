4268c6012502506aaa1139ab6cf77a49d22d0e7c
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-02-18 00:11:23 +0100

    conditionals

--- src/lessons/conditionals.c
+++ src/lessons/conditionals.c
@@ -2,9 +2,9 @@
 
 int main() {
   /* Notes:
-        - a constant is a fixed value that cannot be changed later,
-          unlike most variables.
-        - It's common to use uppercasing for constants.
+        - If an "else if" statement evaluates to false, the program skips to
+          check if the next one is true and so one.
+        - The "else" statement runs if all above conditions were unmet.
   */
 
   double age;
@@ -14,6 +14,12 @@ int main() {
 
   if (age >= 18) {
     printf("You are now signed in!");
+  } else if (age < 0) {
+    printf("Please make sure you are born first!");
+  } else if (age == 0) {
+    printf("You can't sign in, you were just born!");
+  } else {
+    printf("You are too young to sign in!");
   }
 
   return 0;
