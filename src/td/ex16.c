7c46f4a7fc7c13fdb7b458795ba0f4bd099426d7
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-03-21 15:05:44 +0100

    multiplication of two arrays

--- src/td/ex16.c
+++ src/td/ex16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  int i, j, l, c, p, arr[100][100], sum = 0, res[100][100];
+  int i, j, k, l, c, p, sum = 0, res[100][100], arr1[100][100], arr2[100][100];
 
   printf("Entrez la dimension L: ");
   scanf("%d", &l);
@@ -10,50 +10,75 @@ int main() {
   scanf("%d", &c);
 
   printf("Entrez la dimension P: ");
-  scanf("%d", &c);
+  scanf("%d", &p);
 
-  printf("### MATRICE T ###");
+  printf("### MATRICE T ###\n");
 
   for (i = 0; i < l; i++) {
     for (j = 0; j < c; j++) {
       printf("Entrez la valeur [%d][%d]: ", i, j);
-      scanf("%d", &arr[i][j]);
+      scanf("%d", &arr1[i][j]);
     }
   }
-  printf("### MATRICE B ###");
+
+  printf("### MATRICE B ###\n");
 
   for (i = 0; i < c; i++) {
     for (j = 0; j < p; j++) {
       printf("Entrez la valeur [%d][%d]: ", i, j);
-      scanf("%d", &arr[i][j]);
+      scanf("%d", &arr2[i][j]);
     }
   }
 
   for (i = 0; i < l; i++) {
     for (j = 0; j < c; j++) {
-      sum += arr[i][j];
+      sum += arr1[i][j];
     }
   }
 
-  printf("tableau T: [ ");
+  printf("\n\nMatrice T: \n[\n");
 
   for (i = 0; i < l; i++) {
+    printf(" [ ");
     for (j = 0; j < c; j++) {
-      printf("%d ", arr[i][j]);
+      printf("%d ", arr1[i][j]);
     }
+    printf("],\n");
   }
 
-  printf("]");
+  printf("\nMatrice B: \n[\n");
 
-  printf("\nla somme de tout les elements de T est: %d", sum);
+  for (i = 0; i < c; i++) {
+    printf(" [ ");
+    for (j = 0; j < p; j++) {
+      printf("%d ", arr2[i][j]);
+    }
+    printf("],\n");
+  }
+
+  printf("\n]");
+
+  // printf("\n\nla somme de tout les elements de T est: %d", sum);
 
   for (i = 0; i < l; i++) {
     for (j = 0; j < c; j++) {
-      for (j = 0; k < c; j++) {
-        // res[]
+      for (k = 0; k < p; k++) {
+        res[i][j] += (arr1[i][k] * arr2[k][j]);
       }
     }
   }
+
+  printf("\n\nMatrice K: \n[\n");
+
+  for (i = 0; i < l; i++) {
+    printf(" [ ");
+    for (j = 0; j < c; j++) {
+      printf("%d ", res[i][j]);
+    }
+    printf("],\n");
+  }
+
+  printf("\n]");
 
   return 0;
 }
