3947ced0e5f2d3741e292c18f74150f144d11f90
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-03-28 15:33:25 +0000

    refactoring using given variables

--- src/td/td1/ex2.c
+++ src/td/td1/ex2.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main() {
-  int a, b, c, temp;
+  int A, B, C, temp;
 
   printf("Entrez a, b et c: ");
-  scanf("%d %d %d", &a, &b, &c);
+  scanf("%d %d %d", &A, &B, &C);
 
-  temp = a;
-  a = c;
-  c = b;
-  b = temp;
+  temp = A;
+  A = C;
+  C = B;
+  B = temp;
 
-  printf("variables permutees: a=%d b=%d c=%d", a, b, c);
+  printf("variables permutees: A=%d B=%d C=%d", A, B, C);
 
   return 0;
 }
---
+++ src/td/td1/ex3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  int A, B, C, P;
+
+  printf("Entrez A, B et C: ");
+  scanf("%d %d %d", &A, &B, &C);
+
+  return 0;
+}
