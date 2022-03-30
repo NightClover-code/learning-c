6d7c7d76ce9b3667f403aba0d3fd53831a6687af
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-03-30 19:50:13 +0000

    loop training

--- src/td/td1/ex5.c
+++ src/td/td1/ex5.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 
 int main() {
-  int PNET, TVA;
-  double PBRUT;
+  int i, j, l, c, sum = 0, arr[100][100];
 
-  printf("Entrez le prix net de l'article: ");
-  scanf("%d", &PNET);
+  printf("Entrez la dimension L: ");
+  scanf("%d", &l);
 
-  printf("Entrez le TVA: ");
-  scanf("%d", &TVA);
+  printf("Entrez la dimension C: ");
+  scanf("%d", &c);
 
-  PBRUT = PNET + PNET * (TVA / (double)100);
+  for (i = 0; i < l; i++) {
+    for (j = 0; j < c; j++) {
+      printf("Entrez la valeur [%d][%d]: ", i, j);
+      scanf("%d", &arr[i][j]);
+    }
+  }
 
-  printf("PBRUT de l'article: %.2lf", PBRUT);
+  for (i = 0; i < l; i++) {
+    for (j = 0; j < c; j++) {
+      sum += arr[i][j];
+    }
+  }
+
+  printf("tableau: [\n");
+
+  for (i = 0; i < l; i++) {
+    printf(" [ ");
+    for (j = 0; j < c; j++) {
+      printf("%d ", arr[i][j]);
+    }
+    printf("],\n");
+  }
+
+  printf("]\n");
+
+  printf("%d", sum);
 
   return 0;
 }
