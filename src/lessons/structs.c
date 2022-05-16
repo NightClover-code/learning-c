2b7102250142ffeeecc20941ecbd04f0b23e213e
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-05-16 15:00:55 +0100

    good exercice to grasp on structs

--- src/lessons/structs.c
+++ src/lessons/structs.c
@@ -2,25 +2,58 @@
 #include <string.h>
 
 typedef struct {
-  char name[12];
-  int age;
-} Person;
+  int re;
+  int img;
+} Complex;
+
+void printCmplx(char s[], Complex z1) {
+  printf("%s: %d + %di\n", s, z1.re, z1.img);
+}
+
+Complex createCmplx(int re, int img) {
+  Complex z;
+
+  z.re = re;
+  z.img = img;
+
+  return z;
+}
+
+Complex sumComplex(Complex z1, Complex z2) {
+  Complex res;
+
+  res.re = z1.re + z2.re;
+  res.img = z1.img + z2.img;
+
+  return res;
+}
+
+Complex prodCmplx(Complex z1, Complex z2) {
+  Complex res;
+
+  res.re = z1.re * z2.re - z1.img * z2.img;
+  res.img = z1.re * z2.img + z2.re * z1.img;
+
+  return res;
+}
 
 int main() {
-  Person personA;
-  Person personB;
+  int re1, img1, re2, img2;
+  Complex sum, prod;
 
-  strcpy(personA.name, "Joe");  // can't mutate strings, use string copy
-  personA.age = 19;             // mutate numbers directly
+  printf("Entrez le reel et l imaginaire de z1: ");
+  scanf("%d %d", &re1, &img1);
+  Complex z1 = createCmplx(re1, img1);
 
-  strcpy(personB.name, "Alex");
-  personB.age = 18;
+  printf("Entrez le reel et l imaginaire de z2: ");
+  scanf("%d %d", &re2, &img2);
+  Complex z2 = createCmplx(re2, img2);
 
-  printf("Name: %s\t", personA.name);
-  printf("Age: %d\n", personA.age);
+  sum = sumComplex(z1, z2);
+  prod = prodCmplx(z1, z2);
 
-  printf("Name: %s\t", personB.name);
-  printf("Age: %d\n", personB.age);
+  printCmplx("somme", sum);
+  printCmplx("produit", prod);
 
   return 0;
 }
