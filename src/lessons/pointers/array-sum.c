b4557a41b2cf2a557b7a099cd97d15e9f5e32760
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-05-29 11:55:37 +0100

    array sum using pointers

--- src/lessons/pointers/array-sum.c
+++ src/lessons/pointers/array-sum.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int* findMid(int arr[], int len) { return &arr[len / 2]; }
+int pArraySum(int arr[], int len) {
+  int *p, sum = 0;
+
+  for (p = arr; p <= arr + len - 1; p++) {
+    sum += *p;
+  }
+
+  return sum;
+}
 
 int main() {
   /* Notes:
@@ -9,12 +17,11 @@ int main() {
       because the function and its local variables will get destroyed after use.
 */
   int arr[] = {2, 94, 8, 65, 21};
-
   int len = sizeof(arr) / sizeof(arr[0]);
 
-  int* mid = findMid(arr, len);
+  int sum = pArraySum(arr, len);
 
-  printf("%d", *mid);
+  printf("Sum: %d", sum);
 
   return 0;
 }
